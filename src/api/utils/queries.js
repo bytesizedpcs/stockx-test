92fe5a459444069afe717fa259fca790a5b02d14
Austin <austintcanada@hotmail.com>
2019-06-01 12:32:10 -0400

    Updated queries
    Updated queries to be more readable.

--- src/api/utils/queries.js
+++ src/api/utils/queries.js
@@ -1,19 +1,22 @@
 const shoeGet = `
   SELECT * 
-  FROM shoe;
+  FROM shoe_table;
 `;
+
 const shoeGetId = `
   SELECT (sum_shoe_sizes, size_amount) 
-  FROM shoe 
-  WHERE shoe.id=$1;
+  FROM shoe_table 
+  WHERE shoe_table.id=$1;
 `;
+
 const shoePost = `
   INSERT INTO 
-  shoe (name, company, color, sum_shoe_sizes, size_amount)
+  shoe_table (name, company, color, sum_shoe_sizes, size_amount)
   VALUES ($1, $2, $3, $4, $5);
 `;
+
 const createTable = `
-  CREATE TABLE shoe (
+  CREATE TABLE shoe_table (
     id    SERIAL PRIMARY KEY,
     name  VARCHAR(64) NOT NULL,
     company VARCHAR(64),
@@ -22,10 +25,10 @@ const createTable = `
     size_amount INT
   );
 
-  CREATE INDEX ON shoe (id);
+  CREATE INDEX ON shoe_table (id);
 
   INSERT INTO 
-  shoe (name, company, color, sum_shoe_sizes, size_amount)
+  shoe_table (name, company, color, sum_shoe_sizes, size_amount)
   VALUES
   ('350 Boost', 'Yeezy', 'Pirate Black', 250, 30), 
   ('350 Boost', 'Yeezy', 'Turtle Dove', 250, 30),
